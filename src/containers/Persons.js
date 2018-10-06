45b95a99109e17d245e73d50fd2f0ddc44fe4c60
Stas <33718805+StanislavBerezin@users.noreply.github.com>
2018-10-07 00:17:43 +1000

    Update Persons.js

--- src/containers/Persons.js
+++ src/containers/Persons.js
@@ -29,11 +29,4 @@ const mapStateToProps = state => {
     };
 };
 
-const mapDispatchToProps = dispatch => {
-    return {
-        onAddedPerson: (name, age) => dispatch({type: actionTypes.ADD_PERSON, personData: {name: name, age: age}}),
-        onRemovedPerson: (id) => dispatch({type: actionTypes.REMOVE_PERSON, personId: id})
-    }
-};
-
 export default connect(mapStateToProps, mapDispatchToProps)(Persons);
